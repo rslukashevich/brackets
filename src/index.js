a04d5924d63358b64601125a0722e322191ef2d7
Sergey Lukashevich <rs.lukashevich@gmail.com>
2023-02-17 15:05:15 +0300

    init: finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let count;
+  
+  do 
+  {	count = str.length;  
+  	for (let i = 0; i < bracketsConfig.length; i += 1) {
+ 		  str = str.replaceAll(bracketsConfig[i][0]+bracketsConfig[i][1],'');}
+  } while (count !== str.length)
+
+  return (count === 0) ? true : false;
+  
 }
